2c3b6e427de197b67c00ab788f178aeced76c5e6
hurr-son <hprewett05@gmail.com>
2022-10-18 12:31:09 -0500

    Added a display function to display books when added to library

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+let table = document.getElementById('library-table');
+
+
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+
+function Book(title, author, pages, status) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
+    this.status = status
 }
 
 function addBookToLibrary(newBook) {
     myLibrary.unshift(newBook)
-
+    
 }
 
 let book1 = new Book('Cookbook', 'Some Person', '545', 'yes');
@@ -18,9 +22,44 @@ let book2 = new Book('Guide To Stuff', 'Some Guy', '243', 'no');
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
-console.log(myLibrary);
 
 
+function displayLibrary() {
+    
+    for(let book of myLibrary) {
+        // Add to an html table element
+        let row = document.createElement('tr')
+        let id = document.createElement('td')
+        let title = document.createElement('td')
+        let author = document.createElement('td')
+        let pages = document.createElement('td')
+        let status = document.createElement('td')
+        
+        id.innerHTML = `${myLibrary.indexOf(book)}`;
+        title.innerHTML = `${book.title}`;
+        author.innerHTML = `${book.author}`;
+        pages.innerHTML = `${book.pages}`;
+        status.innerHTML = `${book.status}`;
+
+        // Append element to DOM to display on page
+        
+        row.appendChild(id);
+        row.appendChild(title);
+        row.appendChild(author);
+        row.appendChild(pages);
+        row.appendChild(status);
+        table.appendChild(row)
+        
+        
+    }
+
+    
+}
+    
+    
+displayLibrary()
+
+console.log(table)
 
 
 
