2e9e80d49c77e19b201b0dd8d6a653c282fbfff3
hurr-son <hprewett05@gmail.com>
2022-10-26 20:48:30 -0500

    created an array for the checkboxes and one for their id's the id array isn't fuctioning how I want it to yet.
    The goal is to be able to reference the id array when
    the delete button is pressed so only those rows get deleted.

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ let addButton = document.getElementById('addButton');
 let read = '';
 let book = {};
 let myLibrary = [];
+let deleteArray = [];
 let readSelect = '';
 let checkMark = '';
+let checkMarks = [];
 
 
 function Book(title, author, pages, status) {
@@ -32,81 +34,66 @@ function addBookToLibrary(newBook) {
         
 function createTableElements(){  
 
-        row = document.createElement('tr');
-        id = document.createElement('td');
-        title = document.createElement('td');
-        author = document.createElement('td');
-        pages = document.createElement('td');
-        select = document.createElement('td');
-        checkMark = document.createElement('input');
-        checkMark.type = 'checkbox';
-        readSelect = document.createElement('SELECT');
-        let option1 = document.createElement('option');
-        let option2 = document.createElement('option');
-        option1.text = 'Yes';
-        option2.text = 'No';
-        readSelect.add(option1);
-        readSelect.add(option2);
-        
-        console.log(checkMark);
-      }
+    row = document.createElement('tr');
+    id = document.createElement('td');
+    title = document.createElement('td');
+    author = document.createElement('td');
+    pages = document.createElement('td');
+    select = document.createElement('td');
+    checkMark = document.createElement('input');
+    checkMark.type = 'checkbox';
+    checkMark.id = `${book.id}`
+    readSelect = document.createElement('SELECT');
+    let option1 = document.createElement('option');
+    let option2 = document.createElement('option');
+    option1.text = 'Yes';
+    option2.text = 'No';
+    readSelect.add(option1);
+    readSelect.add(option2);
+    console.log(table.rows)
+}
 
 function createDeleteButton(){
-
+        
     rmButton = document.createElement('button');
     rmButton.style.padding = '.1rem';
     rmButton.innerHTML = 'X';
     rmButton.classList.add('remove-button');
 }
-        
-function insertTableData(){
 
-        id.innerHTML = `${book.id}`;
-        title.innerHTML = `${book.title}`;
-        author.innerHTML = `${book.author}`;
-        pages.innerHTML = `${book.pages}`;
-        readSelect.value = read
-        
-    }
+function insertTableData(){
+    
+    id.innerHTML = `${book.id}`;
+    title.innerHTML = `${book.title}`;
+    author.innerHTML = `${book.author}`;
+    pages.innerHTML = `${book.pages}`;
+    readSelect.value = read
+    
+}
 
 function appendTableElements(){ 
-
-        row.appendChild(id);
-        row.appendChild(title);
-        row.appendChild(author);
-        row.appendChild(pages);
-        row.appendChild(readSelect);
-        row.appendChild(select);
-        select.appendChild(checkMark);
-        table.appendChild(row);
-    }
-
-
-function deleteBook() {
-    if(checkMark.checked === true) {
-     return checkMark.parentNode
-    }
-}
- 
     
+    row.appendChild(id);
+    row.appendChild(title);
+    row.appendChild(author);
+    row.appendChild(pages);
+    row.appendChild(readSelect);
+    row.appendChild(select);
+    select.appendChild(checkMark);
+    table.appendChild(row);
+}
+
+
 yesInput.addEventListener('click', function() {
     if(yesInput.checked === true) {
         read = 'Yes'  
-     }
+    }
 })
 noInput.addEventListener('click', function() {
     if(noInput.checked === true) {
         read = 'No'
-     }
+    }
 })
-
-book = new Book('Lord of Rings', 'Tolkien', '545', 'Yes');
-
-addBookToLibrary(book)
-    createTableElements()
-    insertTableData()
-    appendTableElements()
-
 
 addButton.addEventListener('click', function() {
     
@@ -115,6 +102,29 @@ addButton.addEventListener('click', function() {
     createTableElements()
     insertTableData()
     appendTableElements()
+    
 })
 
-console.log(table.children[1].children[0].innerHTML);
+addButton.addEventListener('click', function() {
+    
+    
+    checkMarks = [];
+    checkMarks = document.querySelectorAll('input[type="checkbox"]');
+    console.log(checkMarks);
+
+})
+
+addButton.click()
+
+checkMarks.forEach(input => {input.addEventListener('change', function(){
+    
+
+    if(input.checked === true) {
+
+    deleteArray.push(checkMark.id);
+    console.log(deleteArray);
+    }
+
+    else {}
+
+})})
